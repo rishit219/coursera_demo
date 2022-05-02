a200a8d3560e18882e04dec92b239918225b0a8f
Rishi Thakur <65461105+rishit219@users.noreply.github.com>
2022-05-02 12:30:10 +0530

    Add files via upload
    Uploading file
    (For testing purposes)

---
+++ untitled.py
@@ -0,0 +1,8 @@
+class parent:
+    def __init__(self,fname,lname):
+        self.firstname = fname
+        self.lastname = sname
+    def printInfo(self):
+        print(self.firstname,self.lastname)
+
+onj1 = parent('Arthur','Morgan')
